e3194834b702b848d8525a86fa821f51875a74e3
cc <codecolorist@gmail.com>
2026-03-18 12:49:33 +0100

    only allow single instance

--- Mooring/MooringApp.swift
+++ Mooring/MooringApp.swift
@@ -6,10 +6,12 @@
 //
 
 import SwiftUI
+import AppKit
 
 @main
 struct MooringApp: App {
     @StateObject private var proxyManager = IProxyManager()
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
     var body: some Scene {
         MenuBarExtra("Mooring", systemImage: "cable.connector") {
@@ -27,3 +29,18 @@ struct MooringApp: App {
         .defaultPosition(.center)
     }
 }
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationWillFinishLaunching(_ notification: Notification) {
+        guard let bundleID = Bundle.main.bundleIdentifier else { return }
+        
+        // Fetch all running applications with your bundle ID
+        let runningApps = NSRunningApplication.runningApplications(withBundleIdentifier: bundleID)
+        
+        // If there is more than 1 running, another instance is already active
+        if runningApps.count > 1 {
+            print("Another instance is already running. Terminating.")
+            exit(0)
+        }
+    }
+}
+
